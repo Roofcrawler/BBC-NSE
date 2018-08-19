a5a05f6bc92406328c760cb64d46f54bac980c02
Roofcrawler <roofcrawler@gmail.com>
2018-08-19 15:46:51 +1000

    Changed to add requirements
    getmods.php with correct order of mods to load.

--- BBC-NSE/getmods.php
+++ BBC-NSE/getmods.php
@@ -1,4 +1,16 @@
-nRows=1
+nRows=6
 
-&strModName0=BBC-NSE
-&strModURL0=BBC-NSE
+&strModName0=NSE
+&strModURL0=Mods/NeoScavExtended/NSExtended
+&strModName1=0
+&strModURL1=Mods/NeoScavExtended/NSEoverride
+
+&strModName2=BBC
+&strModURL2=Mods/DevKit/BBCAddOn
+&strModName3=DevKit
+&strModURL3=Mods/DevKit/DevKitAddOn
+&strModName4=0
+&strModURL4=Mods/DevKit/VanillaOverride
+
+&strModName5=BBC-NSE
+&strModURL5=Mods/BBC-NSE/BBC-NSE
